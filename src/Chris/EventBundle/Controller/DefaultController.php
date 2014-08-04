5cfcb7d8789a6756562596c4fe76f88f25601ebf
Christian Schade <bln.schade@googlemail.com>
2014-08-05 00:00:03 +0200

    yip

--- src/Chris/EventBundle/Controller/DefaultController.php
+++ src/Chris/EventBundle/Controller/DefaultController.php
@@ -6,8 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction($name, $count)
     {
-        return $this->render('EventBundle:Default:index.html.twig', array('name' => $name));
+        // var_dump($name,$count);die;
+        return $this->render('EventBundle:Default:index.html.twig', array('name' => $name, 'count' => $count));
+
     }
 }
